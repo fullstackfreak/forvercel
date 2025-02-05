2b72ce1caeeece91a65e29715840ad93f11a4df1
fullstackfreak <sureshkumar.oneteam@gmail.com>
2025-02-05 16:36:55 +0530

    changed the server name in front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,26 @@
 import { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import axios from 'axios'
+import axios from "axios";
 
 function App() {
-  const [name, setName] = useState()
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
-axios.defaults.withCredentials = true;
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  axios.defaults.withCredentials = true;
   const handleSubmit = (e) => {
     e.preventDefault();
-    axios.post('http://localhost:3001/register', {name, email, password})
-    .then(result => console.log(result.data)
-  
-    )
-    .catch(err => console.log(err))
-  }
+    axios
+      .post("forvercel-beta.vercel.app/register", { name, email, password })
+      .then((result) => console.log(result.data))
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
       <div className="bg-white p-3 rounded w-25">
         <h2>Register</h2>
         <form onSubmit={handleSubmit}>
-        <div className="mb-3">
+          <div className="mb-3">
             <label htmlFor="email">
               <strong>Name</strong>
             </label>
@@ -56,7 +55,7 @@ axios.defaults.withCredentials = true;
               placeholder="Enter Password"
               name="password"
               className="form-control rounded-0"
-              onChange={(e) => setPassword(e.target.value)}          
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <button type="submit" className="btn btn-success w-100 rounded-0">
