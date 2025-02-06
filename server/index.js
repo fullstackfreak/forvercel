0e2818f6f5833c90e0a34fcf6ef65358570f6edd
fullstackfreak <sureshkumar.oneteam@gmail.com>
2025-02-06 10:22:54 +0530

    cors changed 8

--- server/index.js
+++ server/index.js
@@ -4,12 +4,36 @@ const cors = require("cors");
 const RegisterModel = require("./models/Register");
 
 const app = express();
-const corsOptions = {
-  origin: "https://forvercel-front.vercel.app/", // Allow only this origin
-  credentials: true, // Allow credentials
+
+// const corsOptions = {
+//   origin: "https://forvercel-front.vercel.app/", // Allow only this origin
+//   credentials: true, // Allow credentials
+// };
+
+// app.use(cors(corsOptions));
+
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return fn(req, res);
 };
 
-app.use(cors(corsOptions));
+const handler = (req, res) => {
+  const d = new Date();
+  res.send(d.toString());
+};
 
 app.use(express.json());
 
@@ -21,18 +45,6 @@ app.get("/", (req, res) => {
   res.json("Hello");
 });
 app.post("/register", (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
   const { name, email, password } = req.body;
   RegisterModel.findOne({ email: email })
     .then((user) => {
