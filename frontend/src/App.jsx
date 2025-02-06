f85f1d9374e919994aee8ed8ba4c9b38bd832f3e
fullstackfreak <sureshkumar.oneteam@gmail.com>
2025-02-06 11:39:59 +0530

    cors changed 20

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,7 +20,8 @@ function App() {
         },
         {
           headers: {
-            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type",
           },
         }
       )
